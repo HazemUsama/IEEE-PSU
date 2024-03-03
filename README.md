707cf3041d99c3dbdcae7d0c82de19417475733d
HazemUsama <hazemosama681@gmail.com>
2024-03-03 16:49:14 +0200

    add main.cpp

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # IEEE-PSU
 Open Source Project
-
-
-This commit is from local
-
-This another commit from local
-
-This commit is from remote
-
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    int average = (a + b + c) / 3;
+    cout << average << "\n";
+    return 0;
+}
